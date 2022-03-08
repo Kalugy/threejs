9b330ef535f99388fd7c4ffe0bac620db75b6e80
Kalugy <danieltrujillo901@gmail.com>
2022-03-07 22:55:08 -0500

    tranform with groups

--- webpack-example/src/script.js
+++ webpack-example/src/script.js
@@ -5,18 +5,31 @@ import * as THREE from 'three'
 const scene = new THREE.Scene()
 
 //Red cube
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color:0xff0000})
-const mesh = new THREE.Mesh(geometry, material)
-//mesh.position.x=0.7
-//mesh.position.y=-0.6
-//mesh.position.z=1
-mesh.position.set(0.7,-0.6,1)
-mesh.scale.set(2,0.5,0.5)
+const groupCubes = new THREE.Group()
+groupCubes.position.set(0,1,0)
+groupCubes.scale.set(1,2,1)
+scene.add(groupCubes) 
 
-//mesh.rotation.reorder('YXZ')
-mesh.rotation.set(0.5,0.5,0)
-scene.add(mesh)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+groupCubes.add(cube1)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00})
+)
+cube2.position.set(2,0,0)
+groupCubes.add(cube2)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff}),
+    
+)
+cube3.position.set(-2,0,0)
+groupCubes.add(cube3)
+
+
 
 //mesh.position.normalize();
 //console.log("test", mesh.position.length())
@@ -39,7 +52,7 @@ camera.position.set(1,1,6)
 scene.add(camera)
 
 //camera.lookAt(new THREE.Vector3(3,0,0))
-camera.lookAt(mesh.position)
+//camera.lookAt(mesh.position)
 
 //console.log("test", mesh.position.distanceTo(camera.position))
 
